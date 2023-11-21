7f65758b05c39e7bbbf627fb47c35ab76b4fc5a7
EmaMSanchez <emamsanchez07@gmail.com>
2023-11-21 20:23:57 -0300

    Correccion key

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -4,8 +4,8 @@ const Proyectos = ({datos}) => {
   return (
     <div className="container flex-row w-[290px] md:w-auto">
     {
-      datos.map((dato) =>(
-      <div className='container flex justify-center gap-4 sombra border-red-600 border-b-[1px]  md:mb-10'>
+      datos.map((dato,index) =>(
+      <div key={index} className='container flex justify-center gap-4 sombra border-red-600 border-b-[1px]  md:mb-10'>
         <div className='container flex-row m-2 md:m-4'>
         <h5 className='font-bold text-[9px] mb-2 md:text-2xl'>{dato.titulo}</h5>
         <p className='font-normal text-[8px] mb-10 md:text-base md:mb-24'>{dato.descripcion}</p>
