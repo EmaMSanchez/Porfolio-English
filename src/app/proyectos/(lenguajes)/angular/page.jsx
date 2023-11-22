77e6114959339eec4494b22c7cca90a198cd8245
EmaMSanchez <emamsanchez07@gmail.com>
2023-11-21 21:11:20 -0300

    Links

--- src/app/proyectos/(lenguajes)/angular/page.jsx
+++ src/app/proyectos/(lenguajes)/angular/page.jsx
@@ -1,17 +1,4 @@
-import Proyectos from "@/components/Proyectos";
-import AngularLogo from "@/components/logos/AngularLogo";
-import { AngularDato } from "@/mock/Items";
 
 export default function Angular(){
-
-const datos = AngularDato;
-
-  return (
-    <main className="flex flex-col items-center  p-12">
-      <div href="/proyectos/angular" className="logo ng  justify-center  mb-24 md:mb-36">
-        <AngularLogo></AngularLogo>
-      </div>
-      <Proyectos datos={datos}></Proyectos>
-    </main>
-  );
+<h1 className="font-extrabold text-5xl">Proximamente</h1>
 }
--- src/app/proyectos/(lenguajes)/react/page.jsx
+++ src/app/proyectos/(lenguajes)/react/page.jsx
@@ -1,7 +1,17 @@
+import Proyectos from "@/components/Proyectos";
+import ReactLogo from "@/components/logos/ReactLogo";
+import { ReactDato } from "@/mock/Items";
+
 export default function React() {
-    return (
-      <main className="flex flex-col items-center p-24">
-      <h1 className="font-extrabold text-5xl">Proximamente</h1>
-     </main>
-    )
+  const datos = ReactDato;
+
+  return (
+   
+    <main className="flex flex-col items-center  p-12">
+      <div href="/proyectos/angular" className="logo react  justify-center  mb-24 md:mb-36">
+       <ReactLogo></ReactLogo>
+      </div>
+      <Proyectos datos={datos}></Proyectos>
+    </main>
+  );
   }
